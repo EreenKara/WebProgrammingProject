a19d4a3a309d23b10a67f5fc1055e26bff0701b7
erenkara <errennkaaraa@hotmail.com>
2023-11-08 14:48:52 +0300

    Add project files.

---
+++ WebProgrammingProject/Controllers/AnaSayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProgrammingProject.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
